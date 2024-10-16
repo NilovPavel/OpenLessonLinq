f6758c0839d41636436ba4147cfc8e4882f0fe2d
Нилов Павел Геннадьевич <nilpg@volhovec.ru>
2024-10-16 13:46:05 +0300

    Добавлен код к методам.

---
+++ Aggregate/Program.cs
@@ -0,0 +1,20 @@
+﻿//Исходный массив
+double[] array = new double[] { 1, 2, 3, 5, 7, 9 };
+
+//Подсчет элементов
+int count = array.Count
+    (   
+        //Также принимает func
+        //item => item > 3
+    );
+
+//Среднее арифметическое
+double midArithm = array.Average();
+
+//Минимальное значение
+double max = array.Max();
+
+//Макимальное значение
+double min = array.Min();
+
+;
---
+++ UniqueOperators/Program.cs
@@ -0,0 +1,10 @@
+﻿//Массив строк
+string[] strings = new string[] { "Alice", "Bов", "Carry", "Dilan", "Frank", "George", "Harold" };
+
+//Первый элемент
+
+
+//Единичный элемент
+
+
+//
