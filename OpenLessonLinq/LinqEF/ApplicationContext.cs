f2a7df2fee709036b6a92003ccd370bdcb0f90d3
Нилов Павел Геннадьевич <nilpg@volhovec.ru>
2024-10-17 13:00:04 +0300

    Добавлен план доклада.

--- OpenLessonLinq/LinqEF/ApplicationContext.cs
+++ OpenLessonLinq/LinqEF/ApplicationContext.cs
@@ -12,14 +12,6 @@ public class ApplicationContext : DbContext
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
-        //optionsBuilder.UseSqlServer("Server=localhost; Database=Users;"
-        //    + "User Id=sa;Password=23011986;"
-        //+"Integrated Security = true;"
-        //+"Trusted_Connection=True;
-        //+"MultipleActiveResultSets=true;"
-        //);
-
         optionsBuilder.UseSqlServer("data source=localhost;initial catalog=Users;user=sa;password=23011986;App=EntityFramework;TrustServerCertificate=True");
-
     } 
 }
---
+++ OpenLessonLinq/UniqueOperators/Program.cs
@@ -0,0 +1,43 @@
+﻿//Массив строк
+string[] strings = new string[] { "Alice", "Bов", "Carry", "Dilan", "Frank", "George", "Harold" };
+
+//Первый элемент
+string first = strings.First(
+    //Если убрать комментарий, то выдаст ошибку, так как нет элементов в коллекции
+    //удовлетворяющих условию
+    //item => item.Length >= 7  
+    );
+string firstOrDefault =
+    strings.FirstOrDefault
+    (
+        item => item.Length >= 7
+    );
+
+//Последний элемент
+string last = strings.Last
+    (
+    //Если убрать комментарий, то выдаст ошибку, так как нет элементов в коллекции
+    //удовлетворяющих условию
+    //item => item.Length >= 7  
+    );
+
+string lastOrDefault =
+    strings.LastOrDefault
+    (
+        item => item.Length >= 7
+    );
+
+//Единичный элемент
+string single = 
+    strings.Single
+    (
+    //Если убрать комментарий, то отработает успешно, так как только 1 элемент
+    //в коллекции удовлетворяющий условию
+    item => item.Length == 3  
+    );
+
+string singleOrDefault =
+    strings.SingleOrDefault
+    (
+        //item => item.Length ==6
+    );
