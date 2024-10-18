0781fe8e38bde2ea50f1447c916351e098db7989
Нилов Павел Геннадьевич <nilpg@volhovec.ru>
2024-10-18 18:38:52 +0300

    Added bonus and DefferedImmediate.

---
+++ OpenLessonLinq/Bonus/Program.cs
@@ -0,0 +1,9 @@
+﻿//Как в LINQ найти индекс элемента с помощью лямбда-выражений?
+int i = 0;
+string[] strings = new string[] { "A", "B", "C", "D", "E" };
+int index = strings
+    .Select(item => new { Symbol = item, Index = i++ } )
+    .Where( item => item.Symbol == "B" )
+    .First()
+    .Index;
+;
---
+++ OpenLessonLinq/ImmediateDeferred/Program.cs
@@ -0,0 +1,24 @@
+﻿//Набор данных
+int[] numbers = { 1, 2, 3, 4, 5 };
+
+// Запрос создается, но не выполняется
+var query = from n in numbers
+            where n > Get1()
+            select n;
+;
+
+// Запрос выполнится только здесь, когда мы начнем итерировать результаты
+foreach (var number in query)
+{
+    Console.WriteLine(number);
+}
+
+//Запрос с немедленным выполнением
+numbers.Where(item => item > Get1()).Count();
+
+;
+
+int Get1()
+{
+    return 2;
+}
